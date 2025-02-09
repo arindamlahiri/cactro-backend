a64978007004878dc2dcee612410e03f971f8191
Arindam Lahiri <arindamlahiri17@gmail.com>
2025-02-09 12:02:39 +0530

    chore: env setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import fastify from 'fastify';
+import fastifyEnv from '@fastify/env';
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 const host = 'RENDER' in process.env ? `0.0.0.0` : `localhost`;
 
 const app = fastify({
 	logger: true
+});
+
+const envSchema = {
+	type: 'object',
+	required: ['TEST'],
+	properties: {
+		TEST: {
+			type: 'string'
+		}
+	}
+};
+
+app.register(fastifyEnv, {
+	dotenv: true,
+	schema: envSchema
 });
 
 app.get('/health-check', async (request, reply) => {
