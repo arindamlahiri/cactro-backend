13a7f65dd362c1717c8658b1793f73d7e4f07479
Arindam Lahiri <arindamlahiri17@gmail.com>
2025-02-09 13:25:12 +0530

    chore: schema update

--- src/schema.ts
+++ src/schema.ts
@@ -1,16 +1,16 @@
 import {
 	boolean,
-	integer,
 	pgTable,
 	serial,
 	text,
-	timestamp
+	timestamp,
+	varchar
 } from 'drizzle-orm/pg-core';
 
 export const cache = pgTable('cache', {
 	id: serial('id').primaryKey(),
-	key: text('key').notNull().unique(),
-	value: text('value').notNull(),
+	key: varchar('key', { length: 1000 }).notNull().unique(),
+	value: varchar('value', { length: 2000 }).notNull(),
 	isDeleted: boolean('is_deleted').notNull().default(false),
 	createdAt: timestamp('created_at').notNull().defaultNow(),
 	updatedAt: timestamp('updated_at')
