bf850c5cf38c3427cf5cff5db873556a9ea93ce9
Arindam Lahiri <arindamlahiri17@gmail.com>
2025-02-09 11:49:11 +0530

    add health-check route

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import fastify from 'fastify';
 
-const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 const host = 'RENDER' in process.env ? `0.0.0.0` : `localhost`;
 
 const app = fastify({
 	logger: true
 });
 
-app.get('/ping', async (request, reply) => {
-	return 'pong\n';
+app.get('/health-check', async (request, reply) => {
+	return 'All good';
 });
 
 app.listen({ host, port }, (err, address) => {
